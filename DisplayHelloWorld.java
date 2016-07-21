3e7705e22ff01245ea9c4f6356cc0e55d2187406
Aishwant <aishwantg@gmail.com>
2016-07-21 14:20:29 -0400

    Add files via upload

---
+++ DisplayHelloWorld.java
@@ -0,0 +1,7 @@
+public class DisplayHelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello World"); //Displays Hello World in a line
+		System.out.print("Hello "); //Displays Hello 
+		System.out.print("World\n");//Displays World in same line and breaks the line
+	}
+}
